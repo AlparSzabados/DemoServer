92695fa9044945241d93939acff63c543def1d0f
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-14 14:10:04 +0300

    Corrected mistake

--- src/main/java/com/alpar/szabados/hibernate/server/repositories/ActivityRepository.java
+++ src/main/java/com/alpar/szabados/hibernate/server/repositories/ActivityRepository.java
@@ -1,7 +1,11 @@
 package com.alpar.szabados.hibernate.server.repositories;
 
-import com.alpar.szabados.hibernate.server.entities.User;
+import com.alpar.szabados.hibernate.server.entities.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActivityRepository extends JpaRepository<User, Long> {
+import java.util.List;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    List<Activity> findActivitiesByUserId(long userId);
 }
