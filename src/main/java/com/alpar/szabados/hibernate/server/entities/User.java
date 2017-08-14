0ab2b2607dfc9c26eeaa2eef851cd119aa76816c
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-14 14:09:12 +0300

    Changed indentation

--- src/main/java/com/alpar/szabados/hibernate/server/entities/User.java
+++ src/main/java/com/alpar/szabados/hibernate/server/entities/User.java
@@ -6,66 +6,66 @@ import java.util.Objects;
 @Entity
 @Table(name = "user")
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id", nullable = false, unique = true)
-	private long userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false, unique = true)
+    private long userId;
 
-	@Column(name = "user_name", nullable = false, unique = true)
-	private String userName;
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
 
-	@Column(name = "password")
-	private String password;
+    @Column(name = "password")
+    private String password;
 
-	public User() {
-	}
+    public User() {
+    }
 
-	public User(String userName, String password) {
-		this.userName = userName;
-		this.password = password;
-	}
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 
-	public long getUserId() {
-		return userId;
-	}
+    public long getUserId() {
+        return userId;
+    }
 
-	public void setUserId(long userId) {
-		this.userId = userId;
-	}
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	@Override
-	public String toString() {
-		return String.format("User{userId=%d, userName='%s', password='%s'}", userId, userName, password);
-	}
+    @Override
+    public String toString() {
+        return String.format("User{userId=%d, userName='%s', password='%s'}", userId, userName, password);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		User user = (User) o;
-		return userId == user.userId &&
-				Objects.equals(userName, user.userName) &&
-				Objects.equals(password, user.password);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId &&
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(password, user.password);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(userId, userName, password);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, userName, password);
+    }
 }
