19ba296c7d3c2e75c37b4e4da9a656ca4b08de31
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-14 14:11:56 +0300

    Changed indentation

--- src/main/java/com/alpar/szabados/hibernate/server/ServerApplication.java
+++ src/main/java/com/alpar/szabados/hibernate/server/ServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.alpar.szabados.hibernate"})
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 }
