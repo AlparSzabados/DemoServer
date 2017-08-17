57e5ef4aa289de6cd5a537bda1415a137bce45f8
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-17 12:45:51 +0300

    Modified query to return User and not list

--- src/main/java/com/alpar/szabados/hibernate/server/repositories/UserRepository.java
+++ src/main/java/com/alpar/szabados/hibernate/server/repositories/UserRepository.java
@@ -3,10 +3,8 @@ package com.alpar.szabados.hibernate.server.repositories;
 import com.alpar.szabados.hibernate.server.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByUserNameAndPassword(String userName, String password);
+    User findByUserName(String userName);
     User findByUserId(long id);
     User findUserByUserName(String name);
 }
