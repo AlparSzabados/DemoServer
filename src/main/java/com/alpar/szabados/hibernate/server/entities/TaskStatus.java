83b72a5f968e65dcd5974c2a2c0a538b5610adb8
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 18:16:54 +0300

    Added `@XmlRootElement` annotation

--- src/main/java/com/alpar/szabados/hibernate/server/entities/TaskStatus.java
+++ src/main/java/com/alpar/szabados/hibernate/server/entities/TaskStatus.java
@@ -1,5 +1,8 @@
 package com.alpar.szabados.hibernate.server.entities;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public enum TaskStatus {
     COMPLETED,
     NOT_COMPLETED
