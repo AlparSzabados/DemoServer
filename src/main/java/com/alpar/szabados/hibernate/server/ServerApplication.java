d225a28677d37131217934f8cc1f9615064db036
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 18:20:32 +0300

    Added password encoder Bean and dependencies

--- src/main/java/com/alpar/szabados/hibernate/server/ServerApplication.java
+++ src/main/java/com/alpar/szabados/hibernate/server/ServerApplication.java
@@ -2,12 +2,20 @@ package com.alpar.szabados.hibernate.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan({"com.alpar.szabados.hibernate"})
 public class ServerApplication {
     public static void main(String... args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
