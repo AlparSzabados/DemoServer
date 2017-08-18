bb59b5f32f1606a8cb11b4e4b8c744b79e3fd7a3
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-18 18:15:35 +0300

    Added wrapper class to deserialize JSON

---
+++ src/main/java/com/alpar/szabados/hibernate/server/entities/UserAndActivityWrapper.java
@@ -0,0 +1,37 @@
+package com.alpar.szabados.hibernate.server.entities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+public class UserAndActivityWrapper implements Serializable{
+
+    private static final long serialVersionUID = -3539523753863844554L;
+
+    private User user;
+    private Activity activity;
+
+    public UserAndActivityWrapper() {
+    }
+
+    public UserAndActivityWrapper(User user, Activity activity) {
+        this.user = user;
+        this.activity = activity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+}
