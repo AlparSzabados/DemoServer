ef4827c867dccda548d01064a7a8b2575b5f1c65
Alpar.Szabados <szabadosalpar@gmail.com>
2017-08-20 15:43:50 +0300

    Removed unused queries

--- src/main/java/com/alpar/szabados/hibernate/server/repositories/UserRepository.java
+++ src/main/java/com/alpar/szabados/hibernate/server/repositories/UserRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserName(String userName);
-    User findByUserId(long id);
-    User findUserByUserName(String name);
 }
